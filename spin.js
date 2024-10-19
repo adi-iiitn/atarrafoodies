777c9f50a51df546cd55f938cdf4cb8514529f7d
ADARSH KASAUNDHAN <ag3187629@gmail.com>
2024-10-19 15:51:57 +0530

    added spin.js

--- spin.js
+++ spin.js
@@ -1,3 +1,3 @@
 setTimeout(() => {
-    window.location.href = " https://adi-iiitn.github.io/af/"; // Redirects to Google
+    window.location.href = "https://www.google.com"; // Redirects to Google
 }, 3000); // 3000 milliseconds = 3 seconds
