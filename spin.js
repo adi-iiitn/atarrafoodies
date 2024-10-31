8b1cbca6f49445966f2429fdd4eea316b07ba357
ADARSH KASAUNDHAN <ag3187629@gmail.com>
2024-10-31 17:04:57 +0530

    Update spin.js

--- spin.js
+++ spin.js
@@ -1,3 +1,3 @@
 setTimeout(() => {
-    window.location.href = "https://amazon.com"; // Redirects to Google
+    window.location.href = "https://amazon.com/"; // Redirects to Google
 }, 3000); // 3000 milliseconds = 3 seconds
