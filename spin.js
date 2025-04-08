0cb01d1bffe434cc3c960fb24838c498028b6984
ADARSH KASAUNDHAN <ag3187629@gmail.com>
2025-04-09 05:11:35 +0530

    Update spin.js

--- spin.js
+++ spin.js
@@ -1,3 +1,3 @@
 setTimeout(() => {
-    window.location.href = "https://atarrafoods.vercel.app/"; // Redirects to Google
+    window.location.href = "https://atarrafoodss.vercel.app/"; // Redirects to Google
 }, 3000); // 3000 milliseconds = 3 seconds
